13603892129e6d34f7dd6e180c8d949866825b28
Sostene Munezero Bagira <munezerobagira@gmail.com>
2023-02-28 19:55:20 +0200

    documentation for singup and login and movies

--- docs/swagger/swagger.ts
+++ docs/swagger/swagger.ts
@@ -20,11 +20,10 @@ export const swaggerDocument = {
         },
       },
     },
-    s,
   ],
   tags: [
     {
-      name: 'sample',
+      name: 'Sample',
       description: 'Endpoints for sample requests',
     },
     {
@@ -33,7 +32,7 @@ export const swaggerDocument = {
     },
     {
       name: 'MovieList',
-      description: 'Endpoints for fetching and getting movies',
+      description: 'Endpoints for fetching and getting movies list',
     },
     {
       name: 'Authentication',
@@ -50,10 +49,114 @@ export const swaggerDocument = {
         tags: ['Authentication'],
         summary: 'Endpoint for logging in',
         operationId: 'login',
-        requestBody: {},
+        requestBody: {
+          description: 'Login credentials',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/login',
+              },
+            },
+          },
+          required: true,
+        },
         responses: {
           '201': {
             description: 'Server is up',
+          },
+        },
+      },
+    },
+    '/auth/signup': {
+      post: {
+        tags: ['Authentication'],
+        summary: 'Endpoint for Sign up',
+        operationId: 'signup',
+        requestBody: {
+          description: 'Login credentials',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/signup',
+              },
+            },
+          },
+          required: true,
+        },
+        responses: {
+          '201': {
+            description: 'Signed up successfully',
+          },
+        },
+      },
+    },
+
+    '/movies': {
+      get: {
+        tags: ['Movies'],
+        summary: 'Endpoint for getting all movies',
+        operationId: 'get-movies',
+        securityShemes: {},
+        responses: {
+          '200': {
+            description: 'Fetching movies successfully',
+          },
+          '404': {
+            description: 'Endpoint is not found',
+          },
+        },
+      },
+    },
+    '/movies/search': {
+      get: {
+        tags: ['Movies'],
+        summary: 'Endpoint for getting all the users',
+        operationId: 'search-movies',
+        parameters: [
+          {
+            name: 'keywords',
+            in: 'query',
+            description: 'Search keywords',
+            required: true,
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
+        securityShemes: {},
+        responses: {
+          '200': {
+            description: 'User were fetched successfully',
+          },
+          '404': {
+            description: 'Endpoint is not found',
+          },
+        },
+      },
+    },
+    '/movies/{movieId}': {
+      get: {
+        tags: ['Movies'],
+        summary: 'Endpoint for getting all the users',
+        operationId: 'get-users',
+        parameters: [
+          {
+            name: 'movieId',
+            in: 'paramater',
+            description: 'Movie id',
+            required: true,
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
+        securityShemes: {},
+        responses: {
+          '200': {
+            description: 'User were fetched successfully',
+          },
+          '404': {
+            description: 'Endpoint is not found',
           },
         },
       },
@@ -70,74 +173,6 @@ export const swaggerDocument = {
           },
           '404': {
             description: 'Endpoint is not found',
-          },
-        },
-      },
-      post: {
-        tags: ['sample'],
-        summary: 'endpoint to test if server is up',
-        operationId: 'signup-user',
-        requestBody: {},
-        responses: {
-          '201': {
-            description: 'Server is up',
-          },
-        },
-      },
-      put: {
-        tags: ['sample'],
-        summary: 'endpoint to test if server is up',
-        operationId: 'sample-put',
-        responses: {
-          '200': {
-            description: 'Server is up',
-          },
-        },
-      },
-      patch: {
-        tags: ['sample'],
-        summary: 'endpoint to test if server is up',
-        operationId: 'sample-patch',
-        responses: {
-          '200': {
-            description: 'Server is up',
-          },
-        },
-      },
-      delete: {
-        tags: ['sample'],
-        summary: 'endpoint to test if server is up',
-        operationId: 'sample-delete',
-        responses: {
-          '200': {
-            description: 'Server is up',
-          },
-        },
-      },
-    },
-    '/movies': {
-      get: {
-        tags: ['Users'],
-        summary: 'Endpoint for getting all the users',
-        operationId: 'get-users',
-        securityShemes: {},
-        responses: {
-          '200': {
-            description: 'User were fetched successfully',
-          },
-          '404': {
-            description: 'Endpoint is not found',
-          },
-        },
-      },
-      post: {
-        tags: ['sample'],
-        summary: 'endpoint to test if server is up',
-        operationId: 'signup-user',
-        requestBody: {},
-        responses: {
-          '201': {
-            description: 'Server is up',
           },
         },
       },
@@ -224,22 +259,6 @@ export const swaggerDocument = {
         },
       },
     },
-    // '/movies': {
-    //   get: {
-    //     tags: ['Movies'],
-    //     summary: 'Endpoint for getting all the users',
-    //     operationId: 'get-movie',
-    //     securityShemes: {},
-    //     responses: {
-    //       '200': {
-    //         description: 'Fetching movies was successfully',
-    //       },
-    //       '404': {
-    //         description: 'Endpoint is not found',
-    //       },
-    //     },
-    //   },
-    // },
   },
   components: {
     schemas: {
@@ -261,6 +280,43 @@ export const swaggerDocument = {
           name: 'John Doe',
           email: 'johndoe@example.com',
           password: 'testing',
+        },
+      },
+      login: {
+        required: ['email', 'password'],
+        type: 'object',
+        properties: {
+          email: {
+            type: 'string',
+          },
+          password: {
+            type: 'string',
+          },
+        },
+        example: {
+          email: 'johndoe@example.com',
+          password: 'password',
+        },
+      },
+
+      signup: {
+        required: ['email', 'password', 'name'],
+        type: 'object',
+        properties: {
+          email: {
+            type: 'string',
+          },
+          password: {
+            type: 'string',
+          },
+          name: {
+            type: 'string',
+          },
+        },
+        example: {
+          email: 'johndoe@example.com',
+          password: 'password',
+          name: 'John doe',
         },
       },
     },
